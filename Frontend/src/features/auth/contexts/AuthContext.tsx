b6f0d03f7dfa7b8eca3d8923cf7383c9f1dff5ed
ntanon <ntphanon@proton.me>
2026-06-16 17:22:45 +0700

    FE/REF!: Migrating frontend (again xd)

--- Frontend/src/features/auth/contexts/AuthContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import type { IAuthContext } from "./auth.t";
-
-export const AuthContext = createContext<IAuthContext | null>(null);
--- Frontend/src/features/course/hooks/useCourses.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCourses } from "@/features/courses/api/courseApi";
-
-export const useCourses = () =>
-  useQuery({
-    queryKey: ["courses"],
-    queryFn: getCourses,
-  });
